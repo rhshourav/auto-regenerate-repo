a2ef16aeec7612b2546f52c6d1cd47115e5b4fe0
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2024-08-14 13:56:12 +0600

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -2,7 +2,7 @@ import datetime
 
 def generate_readme():
     content = f"# Auto Regenerate Repository\n\nThis README was last updated: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
-    content += f"\n ![TryHackMe](https://tryhackme.com/badge/533634)"
+    content += f"\n ![TryHackMe](https://tryhackme-badges.s3.amazonaws.com/deshoha.png)"
 
     with open("README.md", "w") as readme_file:
         readme_file.write(content)
